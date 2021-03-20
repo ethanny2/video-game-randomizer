60f0f4f031f710069966e615a8152f3c0ef481e7
Ethan <ethanny2@gmail.com>
2021-03-20 00:14:34 -0400

    Transitioning to mobile first dev and focusing my styles on that, modified php scrape to include all information, got infinite scroll to work on mobile, created a mobile modal mockup that needs some work and figurued out the solution to close the modal when another place is clicked

--- src/js/random.js
+++ src/js/random.js
@@ -5,13 +5,21 @@ let grid;
 window.addEventListener("DOMContentLoaded", () => {
   generateImages();
   let loader = document.getElementById("lds-default");
+  const offset = 10;
   $(window).on("scroll", function () {
-    if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+    if ($(window).scrollTop() > $(document).height() - $(window).height() - offset) {
       console.log("Loading more infinite images");
       loader.style.display = "initial";
       generateImages();
     }
   });
+  // $(".modal").on("click", function (e) {
+  //   //Prevent event from bubbling up any further
+  //   e.preventDefault();
+  //   e.stopPropagation();
+  //   e.stopImmediatePropagation();
+  //   return false;
+  // });
 });
 async function generateImages() {
   var request = await fetch("http://localhost/game-randomizer-portfolio-update/src/php/queryForImages.php", {
@@ -38,6 +46,7 @@ async function generateImages() {
 function appendImages(links) {
   const gridElem = document.getElementById("grid");
   const loader = document.getElementById("lds-default");
+  const modal = document.getElementsByClassName("modal")[0];
   grid = new Packery(gridElem, {
     columnWidth: ".grid-sizer",
     itemSelector: ".grid-item",
@@ -49,9 +58,40 @@ function appendImages(links) {
   $.each(links, function (_, value) {
     if (value) {
       const gridItem = $("<div></div>");
-      const image = $("<img src=" + value + " />");
+      const image = $("<img src=" + value.cover + " />");
       gridItem.addClass("grid-item");
       gridItem.append(image);
+      gridItem.on("click", (e) => {
+        // if modal is not displayed; show modal prevent bubble
+        // if modal is displayed; let event bubble to body
+        e.preventDefault();
+        e.stopPropagation();
+        e.stopImmediatePropagation();
+        console.log("Grid item clicked");
+        //If modal is visible close 
+        modal.style.display = "initial";
+        modal.classList.add("fade-in");
+        $(modal).html(`
+        <h1>${value.name}</h1>
+        <div class="modal__contents">
+          <div class="modal__image">
+            <img src="${value.cover}" alt="${value.name}" />
+          </div>
+          <div class="modal__time">
+            <p>Main Story: ${value.main_story || "N/A"}</p>
+            <p>Completionist: ${value.completionist || "N/A"}</p>
+            <p>Average: ${value.combined || "N/A"}</p>
+          </div>
+          <div class="modal__info">
+            <p>Release Date: ${value.release_data}</p>
+            <p>Platforms: ${value.platforms}</p>
+            <p>Average Score: ${value.rating || "N/A"} </p>
+            <p>Summary: ${value.summary || "N/A"}</p>
+            <a href="${value.url}">Giant Bomb Page</a>
+          </div>
+        </div>`);
+        return false;
+      });
       image.on("load", () => {
         if (loader.style.display !== "none") loader.style.display = "none";
         //Send it into the packery grid.
--- src/php/queryForImages.php
+++ src/php/queryForImages.php
@@ -13,12 +13,12 @@
   $port = "3307";
 
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
-$randomGameRows = "SELECT cover FROM giant_bomb_games WHERE `cover` IS NOT NULL ORDER BY RAND() LIMIT 20";
+$randomGameRows = "SELECT * FROM giant_bomb_games WHERE `cover` IS NOT NULL ORDER BY RAND() LIMIT 20";
 $result = $conn->query($randomGameRows);
 $imageArray= [];
 if($result->num_rows > 0){
          while($row = $result->fetch_assoc()){
-         	array_push($imageArray, $row["cover"]);
+         	array_push($imageArray, $row);
          }
         // echo var_dump($imageArray);
         echo json_encode($imageArray);
