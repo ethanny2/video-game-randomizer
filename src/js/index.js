ecf253c74deee8809e17dd647fb004cf1f840287
Ethan <ethanny2@gmail.com>
2021-03-22 15:50:12 -0400

    Add intersection observer animations and adjusted the design of the sprites and made it so that the absolutely position images don't cause horizontal overflow. Adjusted the animation duration with media queries so they don't fly across the screen. Need to make the button design and figure out how to display them next

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,35 @@
 import "../sass/main-style.scss";
+import "intersection-observer";
+let counter = 0;
+document.addEventListener("DOMContentLoaded", function (event) {
+  // Your code to run since DOM is loaded and ready
+  createObserver();
+});
+
+function createObserver() {
+  const sliceArr = document.getElementsByClassName("slice");
+  let observer;
+  let options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: [0.5]
+  };
+  observer = new IntersectionObserver(handleIntersect, options);
+  for (let i = 0; i < sliceArr.length; i++) {
+    observer.observe(sliceArr[i]);
+  }
+}
+
+function handleIntersect(entries, observer) {
+  // const cmd = document.getElementsByClassName("typing")[0];
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio > 0.5) {
+      /*Add animation class */
+      const { target } = entry;
+      counter ? target.classList.add("fadeLeft") : target.classList.add("fadeRight");
+      // Only happens once
+      observer.unobserve(target);
+      counter++;
+    }
+  });
+}
